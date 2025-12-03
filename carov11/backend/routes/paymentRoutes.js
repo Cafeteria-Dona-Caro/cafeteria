523526c1ed9e6d6a36a7dce9a04dc1783ad88f24
irgrrd <151906063+irgrrd@users.noreply.github.com>
2025-12-03 12:24:34 -0600

    Delete carov11 directory

--- carov11/backend/routes/paymentRoutes.js
+++
@@ -1,6 +0,0 @@
-﻿const express = require('express');
-const router = express.Router();
-router.post('/create-link', (req, res) => {
-  res.json({ success: true, link: `https://fake-pay.com/${req.body.orderId}` });
-});
-module.exports = router;
